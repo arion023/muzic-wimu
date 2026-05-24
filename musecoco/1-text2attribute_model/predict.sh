186a69c2cad2a2805ba705d994c7d9767494c30e
Arion023 <masikow200@gmail.com>
2026-05-24 22:09:48 +0200

    added pipline for using pregenerted prompts with different genres and moods

--- musecoco/1-text2attribute_model/predict.sh
+++ musecoco/1-text2attribute_model/predict.sh
@@ -1,8 +1,33 @@
-python main.py \
+#!/bin/bash
+
+set -e # stops script if error
+
+declare -a genres=("classical" "country" "jazz" "pop" "rock" "traditional")
+declare -a moods=("angry" "exciting" "fear" "funny" "happy" "lazy" "magnificent" "quiet" "romantic" "sad" "warm")
+
+
+for g in "${genres[@]}"
+do 
+    for m in "${moods[@]}"
+    do 
+    echo "test file"
+    echo "../../../data/prompts/${g}/${m}_${g}_prompts.json"
+    python main.py \
     --do_predict \
     --model_name_or_path=XinXuNLPer/MuseCoco_text2attribute \
-    --test_file=data/predict.json \
+    --test_file="../../../data/prompts/${g}/${m}_${g}_prompts.json" \
     --attributes=data/att_key.json \
     --num_labels=num_labels.json \
-    --output_dir=./tmp \
+    --output_dir="./generation/${g}/${m}" \
     --overwrite_output_dir
+
+    python stage2_pre.py \
+    --test_file="../../../data/prompts/${g}/${m}_${g}_prompts.json" \
+    --predictions="./generation/${g}/${m}/predict_attributes.json" \
+    --probabilites="./generation/${g}/${m}/softmax_probs.json" \
+    --attributes=data/att_key.json \
+    --output_file="../2-attribute2music_model/data/controlability/${g}/infer-${m}.bin"
+
+    done
+done
+
